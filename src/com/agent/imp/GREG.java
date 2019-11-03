92899344090c9d95c4abe57b85df85e48740d5ec
gto <goyafuso.voided@gmail.com>
2019-11-03 17:27:22 -0500

    adding dummy agent

---
+++ src/com/agent/imp/GREG.java
@@ -0,0 +1,81 @@
+package com.agent.imp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.agent.Agent;
+import com.agent.GameUtility;
+import com.checkersgame.CheckersGame;
+import com.checkersgame.Move;
+import com.checkersgame.Piece;
+import com.checkersgame.Player;
+import com.checkersgame.gui.CheckerBoard;
+
+public class GREG implements Agent {
+
+	public Player p;
+	public CheckerBoard board;
+	public CheckersGame game;
+	ArrayList<HashMap<Move,Piece[]>> futureMoveStates;
+	boolean turn =false;//flag
+	Move execute_this;
+	int movecounter=0;
+	
+	@Override
+	public void Init(Player p, CheckersGame game, CheckerBoard board) {
+		this.p=p;
+		this.game=game;
+		this.board=board;
+	}
+
+	@Override
+	public void GetMove() {
+		if(!turn) {
+			turn=true;
+			movecounter=0;
+			futureMoveStates = GameUtility.GetCompletedStates(p, game.boardspot);//get the possible moves
+			EvaluateEndStatesAndSelect();
+		} //dont set turn to false until that last move is taken
+			//need to return the move execute this, if multi jump
+			//then need to separate the move\
+		if(execute_this.multiJump==null) {
+			board.playableSpaces[execute_this.start].AgentsMove(p);
+			board.playableSpaces[execute_this.end].AgentsMove(p);
+			turn=false;
+		} else {
+			board.playableSpaces[execute_this.multiJump[movecounter]].AgentsMove(p);
+			board.playableSpaces[execute_this.multiJump[movecounter+1]].AgentsMove(p);
+			movecounter++;
+			if(movecounter==(execute_this.multiJump.length-1)) {
+				turn=false;
+			}
+		}
+	}
+	/**
+	 * Will set execute_this
+	 */
+	public void EvaluateEndStatesAndSelect() {
+		ArrayList<Move> moves = new ArrayList<Move>();
+		for(HashMap<Move,Piece[]> map : futureMoveStates) {
+			for(Move m : map.keySet()) {
+				moves.add(m);
+			}
+		}
+		System.out.println("DEBUG --> "+moves.size());
+		//random move for now
+		execute_this = moves.get((int) ((Math.random()*(moves.size()))));
+		System.out.println("CHOSE "+execute_this);
+	}
+	@Override
+	public void ClearCollectMoves() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Collect(Move m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
